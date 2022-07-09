fa79886a45fc00b0b90fa084cc5ef5cb8faf2bb0
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2022-07-09 18:17:52 +0800

    adding routes

---
+++ src/routes/api/files.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const filesController = require('../../controllers/filesController')
+const upload = require("../../middlewares/upload");
+
+
+// router.get('/', filesController.getAllFiles) // GET | api/files | display all the files
+// router.get('/:publickey', filesController.getFile) // GET | api/files/:publicKey | get a single file
+router.post('/', upload.single('file'),filesController.createFile) // POST | api/files | creates an file
+// router.delete('/:id', filesController.deleteFile) // DELETE | api/files/:privateKey | Delete a file
+
+module.exports = router
---
+++ tests/routes.test.js
@@ -0,0 +1,33 @@
+const app = require('../index')
+const request = require('supertest')
+const path = '/api/files/'
+
+describe('files routes', () => {
+    it('POST api/files Should accept multipart/form-data requests', async () => {
+        await request(app)
+            .post(`${path}`)
+            .field('name', 'file')
+            .attach('file', 'tests/fixtures/mockData.txt')
+            .set('Content-Type', 'multipart/form-data')
+            .expect(200)
+    })
+    it('POST api/files return a response in JSON format with the following attributes: publicKey & privateKey', async () => {
+        const res = await request(app)
+                .post(`${path}`)
+                .field('name', 'file')
+                .attach('file', 'tests/fixtures/mockData.txt')
+                .set('Content-Type', 'multipart/form-data')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                expect(res.body.publicKey).toBeTruthy()
+                expect(res.body.privateKey).toBeTruthy()
+
+    })
+    // it('GET api/files/:pubkey It should accept “publicKey” as a request parameter and return a response stream with a MIME type representing the actual file format.', async () => {
+    //     const res = await request(app)
+    //             .get(`${path}:62d0ea90-6119-4dda-a0ee-72693ac8b007-pub`)
+    //             .expect(200)
+    //             // expect(res.body.mimetype).toBeTruthy()
+
+    // })
+})
